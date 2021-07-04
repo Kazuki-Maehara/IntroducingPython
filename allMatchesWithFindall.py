36f05169fd6f75eafec913f3e000d8f00413fa0a
Kazuki Maehara <treize.citron13@gmail.com>
2021-07-04 11:43:50 +0900

    added an example

---
+++ allMatchesWithFindall.py
@@ -0,0 +1,14 @@
+
+import re
+
+source = 'Young Frankenstein'
+
+m = re.findall('n', source)
+print(m)
+print('Found', len(m), 'matches')
+
+m = re.findall('n.', source)
+print(m)
+
+m = re.findall('n.?', source)
+print(m)
