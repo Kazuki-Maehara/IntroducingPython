c55e1671e62ae78abb3edd618e5a61617ad8d2c4
Kazuki Maehara <treize.citron13@gmail.com>
2021-10-31 18:30:08 +0900

    modified some filenames

--- 9_TheWeb,Untangled/Requests_Library.py
+++
@@ -1,9 +0,0 @@
-import requests
-url = 'URL_TO_API'
-sp = 30
-
-resp = requests.get(url)
-print(resp)
-print('-' * sp)
-
-print(resp.text)
--- 9_TheWeb,Untangled/bottle3.py
+++
@@ -1,9 +0,0 @@
-from bottle import route, run
-
-
-@route('/echo/<thing>')
-def echo(thing):
-    return "Say hello to my little friend: %s!" % thing
-
-
-run(host='localhost', port=8000)
