038e6e33dfb5f14d7489660d968e29d97ca88f9d
Kazuki Maehara <treize.citron13@gmail.com>
2022-01-08 23:07:13 +0900

    added an example

---
+++ APDX_C_PySci/zerosOnesRandoms.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+
+a = np.zeros((3, ))
+
+print(a)
+print(a.ndim)
+print(a.shape)
+print(a.size)
+
+b = np.zeros((2, 4))
+print(b)
+print(b.ndim)
+print(b.size)
+print(b.shape)
+
+k = np.ones((3, 5))
+print(k)
+
+m = np.random.random((3, 5))
+print(m)
