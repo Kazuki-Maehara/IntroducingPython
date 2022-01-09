6a237de40e2cadce97d04751a6b5bfb8dff22fe0
Kazuki Maehara <treize.citron13@gmail.com>
2022-01-09 16:08:58 +0900

    added examples

---
+++ APDX_C_PySci/mathFunctions.py
@@ -0,0 +1,35 @@
+import math
+
+
+print(math.pi)
+print(math.e)
+
+print(math.fabs(-271.1))
+
+print(math.floor(271.1))
+print(math.ceil(98.6))
+
+print(math.factorial(0))
+print(math.factorial(1))
+print(math.factorial(10))
+
+print(math.log(1.0))
+print(math.log(math.e))
+
+print(math.log(8, 2))
+
+print(math.pow(2, 3))
+
+print(2**3)
+
+print(math.sqrt(100.0))
+
+x = 3.0
+y = 4.0
+print(math.hypot(x, y))
+
+print(math.sqrt(x*x + y*y))
+
+print(math.radians(180.0))
+
+print(math.degrees(math.pi))
---
+++ APDX_C_PySci/nparrayWithArange.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+a = np.arange(10)
+
+print(a)
+
+print(a.ndim)
+
+print(a.size)
+
+print(a.shape)
+
+a = np.arange(7, 11)
+
+print(a)
+
+f = np.arange(2.0, 9.8, 0.3)
+print(f)
+
+
+g = np.arange(10, 4, -1.5, dtype=float)
+print(g)
